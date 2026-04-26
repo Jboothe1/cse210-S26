2f522de2dcf8d29b7cbcc8b794f764e57745a322
Jboothe1 <boo22005@byui.edu>
2026-04-25 20:43:53 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Please input your first name");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("Please input your last name");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
-}
+    }
